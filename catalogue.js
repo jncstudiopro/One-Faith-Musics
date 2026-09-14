ccabf71dd50e3e9373b4634bf63d5189f7b9d7c8
jncstudiopro <jnc.studiopro@gmail.com>
2026-09-14 12:07:07 -0400

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -81,7 +81,9 @@ window.JEHOVAH_CATALOGUE = [
       "fr": {
         "releaseDate": "2026-09-08",
         "youtube": null,
-        "audio": null,
+        "audio": null
+        "audioId": "repose-toi-fr"
+        "r2Path": "fr/une-meme-foi/2-Repose-toi, mon ami.mp3",
         "karaokeYoutube": null,
         "karaokeAudio": null,
         "lyrics": null,
